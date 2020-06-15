aff0d784fb5d61e551bab6c11572b27073aff189
Adrian Marquis <adrian.marquis78@gmail.com>
2020-06-15 17:50:01 -0400

    Final rendition of random dog app

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function getDogImage(numOfDogs = 3) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+     $('.results').html(`<h2>Look at these dogs!</h2>`);
+  
+     for (let dog of responseJson.message) {
+       $('.results').append(
+       `<img src="${dog}" class="results-img" width="200" height="auto">`);
+     }
+    $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        let numOfDogs = $('input[name="numOfDogs"]').val();
+        getDogImage(numOfDogs);
+    });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
